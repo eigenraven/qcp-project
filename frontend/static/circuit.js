3bb9355385bd22f2877db47f675b12d2f23ff7c3
Mia Dobson <mia@yunru.se>
2020-02-04 15:38:45 +0000

    Wrangle zero-kets automatically
    There's an drug joke to be made there somewhere. In any case, the zero-ket starting each qubit is MathJax, so we have to manually add the initial ones and use some technical debt trickery.

--- frontend/static/circuit.js
+++ frontend/static/circuit.js
@@ -1,22 +1,39 @@
-console.log('what the')
-var circuits = Array()
-
 $(function(){
+  var circuits = Array()
+  $('.qubit').each(el => {
+    circuits.push({
+      el: el
+    })
+  });
+  $('#qubit-count').text(circuits.length)
+  
+  var HTML_QUBIT = `
+    <div class='qubit'>
+      <div class='info'>
+        %N
+      </div>
+      <div class='dirac'>
+        %KET0
+      </div>
+    </div>`
   $('#qubit-more').click(function(){
     console.log('more')
+    var qnum = circuits.length
     circuits.push({
-      el: $('<div/>', {
-        "class": "qubit"
-      }).appendTo('#qubits')
+      el: $(
+        HTML_QUBIT
+        .replace('%N', String(qnum))
+        .replace('%KET0', $('.qubit .dirac')[0].innerHTML)
+      ).appendTo('#qubits')
     })
     $('#qubit-count').text(circuits.length)
   })
-  .click().click()
 
   $('#qubit-less').click(function(){
     var line
     console.log('fewer')
-    if (line = circuits.pop()) {
+    if (circuits.length > 0) {
+      var line = circuits.pop()
       var lineIsEmpty = true
       if (lineIsEmpty) {
         line.el.remove()
