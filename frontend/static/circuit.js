17195c6d5b18ccbd0c6b6177972fc43561b17895
Mia Dobson <mia@yunru.se>
2020-02-04 15:01:56 +0000

    Qubit visual element

--- frontend/static/circuit.js
+++ frontend/static/circuit.js
@@ -2,23 +2,28 @@ console.log('what the')
 var circuits = Array()
 
 $(function(){
-
   $('#qubit-more').click(function(){
     console.log('more')
     circuits.push({
-    
+      el: $('<div/>', {
+        "class": "qubit"
+      }).appendTo('#qubits')
     })
     $('#qubit-count').text(circuits.length)
   })
   .click().click()
 
   $('#qubit-less').click(function(){
+    var line
     console.log('fewer')
-    var line = circuits.pop()
-    // TODO: Check if line is in use!
-    if (!line.inUse) {
-      circuits.push(line)
+    if (line = circuits.pop()) {
+      var lineIsEmpty = true
+      if (lineIsEmpty) {
+        line.el.remove()
+      } else {
+        circuits.push(line)
+      }
+      $('#qubit-count').text(circuits.length)
     }
-    $('#qubit-count').text(circuits.length)
-  })s
+  })
 })
