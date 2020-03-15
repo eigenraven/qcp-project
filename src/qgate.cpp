5259e857e1314a03aa1b6f417c0a93787631f6f9
Mia Dobson <mia@yunru.se>
2020-03-15 19:58:29 +0000

    Fix mistaken believe of C++' abilities

--- src/qgate.cpp
+++ src/qgate.cpp
@@ -13,11 +13,11 @@ QGate ID(1,{1,0,
 			0,1});
 QGate X(1,{	0,1,
 			1,0});
-QGate Y(1,{	0,-1_i,
-			1_i,0});
-QGate Z(1,{	1,0,
+QGate Y(1,{	0,  -1_i,
+			1_i, 0});
+QGate Z(1,{	1, 0,
 			0,-1});
-QGate H(1,{	1/sqrt(2),1/sqrt(2),
+QGate H(1,{	1/sqrt(2), 1/sqrt(2),
 			1/sqrt(2),-1/sqrt(2)});
 QGate S(1,{	1,0,
 			0,1_i});
@@ -25,10 +25,10 @@ QGate T(1,{	1,0,
 			0,complex{1/sqrt(2),1/sqrt(2)}});
 QGate Tinv(1,{	1,0,
 				0,complex{1/sqrt(2),-1/sqrt(2)}});
-QGate V(1,{	complex{0.5,0.5},complex{0.5,-0.5},
-			complex{0.5,-0.5},complex{0.5,0.5}});
-QGate Vinv(1,{	complex{0.5,-0.5},complex{0.5,0.5},
-				complex{0.5,0.5},complex{0.5,-0.5}});
+QGate V(1,{	complex{0.5, 0.5},complex{0.5,-0.5},
+			complex{0.5,-0.5},complex{0.5, 0.5}});
+QGate Vinv(1,{	complex{0.5,-0.5},complex{0.5, 0.5},
+				complex{0.5, 0.5},complex{0.5,-0.5}});
 
 
 QGate CNOT(2,{	1,0,0,0,
@@ -60,26 +60,41 @@ QGate CCNOT(3,{	1,0,0,0,0,0,0,0,
 // clang-format on
 
 std::optional<QGate*> getGate(std::string gateID) {
-  switch(gateID){
-	  case "id":
-	  case "nop": 	return &ID;
+    if (gate == "id" || gate == "nop") {
+    return &ID;
+  } else if (gate == "x") {
+    return &X;
+  } else if (gate == "y") {
+    return &Y;
+  } else if (gate == "z") {
+    return &Z;
+  } else if (gate == "h" || gate == "hadamard") {
+    return &H;
+  } else if (gate == "s") {
+    return &S;
+  } else if (gate == "t") {
+    return &T;
+  } else if (gate == "tinv") {
+    return &Sinv;
+  } else if (gate == "v") {
+    return &V;
+  } else if (gate == "vinv") {
+    return &Vinv;
+  } else if (gate == "h" || gate == "hadamard") {
+    return &S
 
-	  case "x": 	return &X;
-	  case "y": 	return &Y;
-	  case "z":		return &Z;
-	  case "h":		return &H;
-	  case "s": 	return &S;
-	  case "t": 	return &T;
-	  case "tinv":  return &Tinv;
-	  case "v":     return &Vinv;
-	  case "vinv":  return &Vinv;
+  } else if (gate == "cnot") {
+    return &CNOT;
+  } else if (gate == "cy") {
+    return &CY;
+  } else if (gate == "cz") {
+    return &CZ;
+  } else if (gate == "swap") {
+    return &SWAP;
 
-	  case "cnot":  return &CNOT;
-	  case "cy": 	return &CY;
-	  case "cz":	return &CZ;
-	  case "swap":	return &SWAP;
-	  case "ccnot":	return &CCNOT;
-  }
-  return std::nullopt
+  } else if (gate == "ccnot" || gate == "toffoli") {
+    return &CCNOT;
+  } else
+    return std::nullopt;
 }
 } // namespace qc
