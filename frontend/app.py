1685d0ccca75616137db66d8d61a6453ab245b05
Mia Dobson <mia@yunru.se>
2020-03-16 16:09:59 +0000

    Simplify gate.json interpreter massively

--- frontend/app.py
+++ frontend/app.py
@@ -1,6 +1,21 @@
+import json
+from pathlib import Path
+
 from flask import Flask, render_template
 
-from circuit import OPERATIONS
+_GATES_PATH = Path(__file__).parent.parent / "src" / "qgate.json"
+
+OPERATIONS = {}  # op.name -> op
+with open(_GATES_PATH, encoding="utf8") as f:
+    for name, data in json.load(f).items():
+        OPERATIONS[name] = dict(
+            mathjax={
+                "factor": data.pop("factor", ""),
+                "matrix": "\\\\".join(data.pop("matrix", ["\\text{Not a gate}"]))
+            },
+            name=data.pop("name", "No name"),
+            description=data.pop("description", ""),
+            **data)
 
 app = Flask(__name__, static_url_path='/static')
 
