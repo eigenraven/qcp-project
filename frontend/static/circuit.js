8c8f18bef5de91a2a1c4075d71b21629a34b9d1b
Mia Dobson <mia@yunru.se>
2020-03-16 17:44:00 +0000

    Fix qubit changing

--- frontend/static/circuit.js
+++ frontend/static/circuit.js
@@ -1,35 +1,47 @@
 $(function(){
-  var circuits = Array()
-  $('.qubit').each(el => {
+  circuits = Array()
+  $('.qubit').each((i, el) => {
     circuits.push({
       el: el
     })
   });
   $('#qubit-count').text(circuits.length)
 
+  var ket0
+  function get_ket0(){
+    if( !ket0 ){
+      console.log('Initial |0> MathJax found')
+      ket0 = $('.ket0')[0].innerHTML
+    }
+  }
+
   var HTML_QUBIT = `
     <div class='qubit'>
       <p class='info'>
         qubit %N
       </p>
-      <div class='dirac'>
+      <div class='ket0'>
         %KET0
       </div>
     </div>`
+
+
   $('#qubit-more').click(function(){
+    get_ket0()
     console.log('more')
     var qnum = circuits.length
     circuits.push({
       el: $(
         HTML_QUBIT
         .replace('%N', String(qnum))
-        .replace('%KET0', $('.qubit .dirac')[0].innerHTML)
+        .replace('%KET0', ket0)
       ).appendTo('#qubits')
     })
     $('#qubit-count').text(circuits.length)
   })
 
   $('#qubit-less').click(function(){
+    get_ket0()
     var line
     console.log('fewer')
     if (circuits.length > 0) {
